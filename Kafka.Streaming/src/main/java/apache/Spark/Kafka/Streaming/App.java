5a9af980c65cd626893bd3e937bfb67a681db643
lazadjole <62203309+lazadjole@users.noreply.github.com>
2020-03-18 13:08:40 +0100

    Add files via upload
    Firs commit

---
+++ Kafka.Streaming/src/main/java/apache/Spark/Kafka/Streaming/App.java
@@ -0,0 +1,13 @@
+package apache.Spark.Kafka.Streaming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
